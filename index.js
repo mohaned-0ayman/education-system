2ad313796628c5bd1de3c04bf440288e81c59401
mohamed <mohamedaymanzolfy@gmail.com>
2023-07-27 10:45:41 -0700

    my changes

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ app.use(express.static(path.join(__dirname, "public")));
 
 // Home route
 app.get("/", (req, res) => {
-  res.render("login");
+  res.render("home.ejs");
 });
 
 // Listening for incoming requests
